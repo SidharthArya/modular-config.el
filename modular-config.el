84a84344871cd3754c3e0bd91f61b5c0b3c41048
Arya <arya@archlinux.Archlinux>
2020-08-24 10:02:56 +0530

    FIX: fill-column for commentary

--- modular-config.el
+++ modular-config.el
@@ -27,10 +27,11 @@
 
 ;;; Commentary:
 
-;; modular-config.el package allows you to create custom configurations from .el files contained in a specific folder.
-;; These .el files can be loaded if needed using `modular-config-load` function.
-;; Or it can be loaded from the cli as `emacs --modules "space separated list of modules".
-;; You can also specify module configurations
+;; modular-config.el package allows you to create custom configurations from
+;; .el files contained in a specific folder.  These .el files can be loaded if
+;; needed using `modular-config-load` function.  Or it can be loaded from the
+;; cli as `emacs --modules "space separated list of modules".  You can also
+;; specify module configurations
 
 ;;; Code:
 
